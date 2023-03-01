720bfeef9cc6eda03ce02bb949d36bd961094a75
Gandinator123 <rohangandhi7@icloud.com>
2023-03-01 15:59:30 +0000

    hihihi

---
+++ include/knob.hh
@@ -0,0 +1,22 @@
+#ifndef knob_hh
+#define knob_hh
+
+#include "knob.hh"
+#include <Arduino.h>
+
+class Knob
+{
+public:
+    Knob();
+
+    uint8_t get_rotation();
+
+    void update_rotation(uint8_t keymatrix);
+
+    void set_limits(uint8_t bottom_limit, uint8_t top_limit);
+
+private:
+    uint8_t rotation, max, min, previous;
+};
+
+#endif
---
+++ src/knob.cpp
@@ -0,0 +1,48 @@
+#include "..\include\knob.hh"
+
+// constructor
+Knob::Knob()
+{
+    rotation = 0;
+    max = 0;
+    min = 0;
+    previous = 0;
+}
+
+uint8_t Knob::get_rotation()
+{
+    return __atomic_load_n(&rotation, __ATOMIC_RELAXED);
+}
+
+void Knob::update_rotation(uint8_t keymatrix)
+{
+    // uint8_t current_rotation = get_rotation();
+
+    xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+    uint8_t current_rotation = rotation;
+    xSemaphoreGive(keyArrayMutex);
+
+    if ((previous == 0 && keymatrix == 1) || (previous == 3 && keymatrix == 2) || (previous == 3 && keymatrix == 0) || (previous == 0 && keymatrix == 1))
+    {
+        if (current_rotation < max)
+        {
+            current_rotation += 1;
+        }
+    }
+    else if ((previous == 1 && keymatrix == 0) || (previous == 2 && keymatrix == 3))
+    {
+        if (current_rotation > min)
+        {
+            current_rotation -= 1;
+        }
+    }
+    previous = keymatrix;
+
+    __atomic_store_n(&rotation, current_rotation, __ATOMIC_RELAXED);
+}
+
+void Knob::set_limits(uint8_t bottom_limit, uint8_t top_limit)
+{
+    max = top_limit;
+    min = bottom_limit;
+}
