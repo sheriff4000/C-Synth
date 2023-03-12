63cdcaf5a9815a3921c984eeebd29c6c525bdd9a
Gandinator123 <rohangandhi7@icloud.com>
2023-03-12 11:20:51 +0000

    hi

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <STM32FreeRTOS.h>
 #include "../include/knob.hh"
 #include <ES_CAN.h>
+
+
 // mutex to handle synchronization bug
 SemaphoreHandle_t keyArrayMutex;
 SemaphoreHandle_t notesArrayMutex;
@@ -35,11 +37,11 @@ const int DRST_BIT = 4;
 const int HKOW_BIT = 5;
 const int HKOE_BIT = 6;
 
-//handshaking variables
+// handshaking variables
 volatile bool east;
 volatile bool west;
 
-//keyboard defining variables
+// keyboard defining variables
 volatile int keyboardIndex;
 volatile int numberOfKeyboards;
 
@@ -110,48 +112,57 @@ void sampleISR()
 {
   static uint32_t readCtr = 0;
 
-  if (readCtr == SAMPLE_BUFFER_SIZE) {
+  if (readCtr == SAMPLE_BUFFER_SIZE)
+  {
     readCtr = 0;
     writeBuffer1 = !writeBuffer1;
     xSemaphoreGiveFromISR(sampleBufferSemaphore, NULL);
-    }
-    
+  }
+
   if (writeBuffer1)
     analogWrite(OUTR_PIN, sampleBuffer0[readCtr++]);
   else
     analogWrite(OUTR_PIN, sampleBuffer1[readCtr++]);
 }
 
-void sampleGenerationTask(void *pvParameters) {
+void sampleGenerationTask(void *pvParameters)
+{
   // BUFFER SIZE: 100; INITIATION INTERVAL = 4.5ms
   global_Vout = 0;
   uint32_t lower_phases[12] = {0};
   uint32_t middle_phases[12] = {0};
   uint32_t upper_phases[12] = {0};
   uint64_t ss;
-  while(1){
+  while (1)
+  {
     xSemaphoreTake(sampleBufferSemaphore, portMAX_DELAY);
-    for (uint32_t writeCtr = 0; writeCtr < SAMPLE_BUFFER_SIZE; writeCtr++) {
+    for (uint32_t writeCtr = 0; writeCtr < SAMPLE_BUFFER_SIZE; writeCtr++)
+    {
       // TODO check if g_note_states is ok to be accessed here
 
-      //doing lowe keyboard
+      // doing lowe keyboard
       ss = g_ss;
-      uint32_t Vout = 0; 
+      uint32_t Vout = 0;
       uint8_t volume = knob3.get_rotation();
 
-      if(keyboardIndex == 0){
-        for (int i=0; i<12; ++i) {
-          if (ss & 1) {
+      if (keyboardIndex == 0)
+      {
+        for (int i = 0; i < 12; ++i)
+        {
+          if (ss & 1)
+          {
             lower_phases[i] += (stepSizes[i] >> 1) + bendStep;
             Vout += ((lower_phases[i] >> 24) - 128) >> (8 - volume);
           }
 
-          if (ss & 0x1000) {
+          if (ss & 0x1000)
+          {
             middle_phases[i] += stepSizes[i] + bendStep;
             Vout += ((middle_phases[i] >> 24) - 128) >> (8 - volume);
           }
 
-          if (ss & 0x1000000) {
+          if (ss & 0x1000000)
+          {
             upper_phases[i] += (stepSizes[i] << 1) + bendStep;
             Vout += ((upper_phases[i] >> 24) - 128) >> (8 - volume);
           }
@@ -161,10 +172,10 @@ void sampleGenerationTask(void *pvParameters) {
 
         global_Vout = Vout;
       }
-      else{
+      else
+      {
         Vout = global_Vout;
       }
-      
 
       if (writeBuffer1)
         sampleBuffer1[writeCtr] = Vout + 128;
@@ -174,27 +185,32 @@ void sampleGenerationTask(void *pvParameters) {
   }
 }
 
-void scanOtherBoardsTask(void *pvParameters){
-  //CAN Message Variables
+void scanOtherBoardsTask(void *pvParameters)
+{
+  // CAN Message Variables
   uint32_t ID;
-  uint8_t RX_Message[8]={0};
-  
-  //Timing for the task
+  uint8_t RX_Message[8] = {0};
+
+  // Timing for the task
   const TickType_t xFrequency = 20 / portTICK_PERIOD_MS;
   TickType_t xLastWakeTime = xTaskGetTickCount();
-  while(1){
+  while (1)
+  {
     vTaskDelayUntil(&xLastWakeTime, xFrequency);
-    while (CAN_CheckRXLevel()){
+    while (CAN_CheckRXLevel())
+    {
       CAN_RX(ID, RX_Message);
       xSemaphoreTake(notesArrayMutex, portMAX_DELAY);
-      if(keyboardIndex == 0){
-        g_note_states[RX_Message[0]] = ((RX_Message[3] & 0xf)  << 8) + ((RX_Message[2] & 0xf) << 4) + (RX_Message[1] & 0xf);
+      if (keyboardIndex == 0)
+      {
+        g_note_states[RX_Message[0]] = ((RX_Message[3] & 0xf) << 8) + ((RX_Message[2] & 0xf) << 4) + (RX_Message[1] & 0xf);
       }
-      else{
-        g_note_states[RX_Message[0]] = ((RX_Message[3] & 0xf)  << 8) + ((RX_Message[2] & 0xf) << 4) + (RX_Message[1] & 0xf); // change this to global vout
+      else
+      {
+        g_note_states[RX_Message[0]] = ((RX_Message[3] & 0xf) << 8) + ((RX_Message[2] & 0xf) << 4) + (RX_Message[1] & 0xf); // change this to global vout
       }
       xSemaphoreGive(notesArrayMutex);
-    } 
+    }
   }
 }
 
@@ -207,7 +223,7 @@ void updateDisplayTask(void *pvParameters)
   while (1)
   {
     vTaskDelayUntil(&xLastWakeTime, xFrequency);
-   
+
     // Update display
     u8g2.clearBuffer();                 // clear the internal memory
     u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
@@ -215,19 +231,16 @@ void updateDisplayTask(void *pvParameters)
 
     // print volume
     u8g2.print(knob3.get_rotation_atomic(), DEC);
-    //print east or west
+    // print east or west
 
     u8g2.print(numberOfKeyboards, DEC);
     u8g2.print(keyboardIndex, DEC);
-    
-
-
 
     u8g2.setCursor(2, 30);
     xSemaphoreTake(notesArrayMutex, portMAX_DELAY);
     u8g2.print(g_ss, HEX);
     xSemaphoreGive(notesArrayMutex);
-    
+
     // note showing
     // u8g2.drawStr(2, 30, notes[note]);
 
@@ -247,7 +260,7 @@ void scanKeysTask(void *pvParameters)
   uint8_t keymatrix, current_rotation;
   uint16_t toAnd, keys;
   bool pressed;
-  uint8_t TX_Message[8]={0};
+  uint8_t TX_Message[8] = {0};
   while (1)
   {
     vTaskDelayUntil(&xLastWakeTime, xFrequency);
@@ -266,10 +279,10 @@ void scanKeysTask(void *pvParameters)
     for (int i = 0; i < 7; ++i)
     {
       setRow(i);
-      digitalWrite(REN_PIN,1);          //Enable selected row
+      digitalWrite(REN_PIN, 1); // Enable selected row
       delayMicroseconds(3);
       keyArray[i] = readCols();
-      digitalWrite(REN_PIN,0);
+      digitalWrite(REN_PIN, 0);
     }
     xSemaphoreGive(keyArrayMutex);
 
@@ -301,53 +314,58 @@ void scanKeysTask(void *pvParameters)
     TX_Message[2] = msg_states & 0xf;
     msg_states = msg_states >> 4;
     TX_Message[3] = msg_states & 0xf;
-    if(numberOfKeyboards > 1){
-       CAN_TX(0x123, TX_Message);
+    if (numberOfKeyboards > 1)
+    {
+      CAN_TX(0x123, TX_Message);
     }
-   
+
     // TODO check if need to use mutex here
     knob3.update_rotation(keymatrix);
 
     __atomic_store_n(&bendStep, 2 * 8080 * (512 - analogRead(JOYX_PIN)), __ATOMIC_RELAXED);
 
-    xSemaphoreTake(notesArrayMutex, portMAX_DELAY);    
-    g_note_states[0] = note_states;// need to protect this at some point!!
+    xSemaphoreTake(notesArrayMutex, portMAX_DELAY);
+    g_note_states[0] = note_states; // need to protect this at some point!!
     g_ss = (uint64_t)g_note_states[0] + ((uint64_t)g_note_states[1] << 12) + ((uint64_t)g_note_states[2] << 24);
     xSemaphoreGive(notesArrayMutex);
   }
 }
 
-
-void handShake(){
+void handShake()
+{
 
   uint32_t startup = millis();
   uint32_t current = startup;
-  while (current < startup+3000)//for 4 seconds
+  while (current < startup + 3000) // for 4 seconds
   {
     current = millis();
     // use mutex to access keyarray
     for (int i = 0; i < 7; ++i)
     {
       setRow(i);
-      if(i==5||i==6){
-        digitalWrite(OUT_PIN,1);
+      if (i == 5 || i == 6)
+      {
+        digitalWrite(OUT_PIN, 1);
       }
-       //Set value to latch in DFF
-      digitalWrite(REN_PIN,1);          //Enable selected row
+      // Set value to latch in DFF
+      digitalWrite(REN_PIN, 1); // Enable selected row
       delayMicroseconds(3);
       keyArray[i] = readCols();
-      digitalWrite(REN_PIN,0);
+      digitalWrite(REN_PIN, 0);
     }
-    if(!(keyArray[5] & 0b1000)){
+    if (!(keyArray[5] & 0b1000))
+    {
       west = true;
     }
-    if(!(keyArray[6] & 0b1000)){
+    if (!(keyArray[6] & 0b1000))
+    {
       east = true;
     }
   }
-  //transmit 1 if west and east. else transmit 0 or 2.
-  uint8_t TX_Message[8]={0};
-  if(west && east){
+  // transmit 1 if west and east. else transmit 0 or 2.
+  uint8_t TX_Message[8] = {0};
+  if (west && east)
+  {
     TX_Message[0] = 1;
     CAN_TX(0x123, TX_Message);
     delay(500);
@@ -356,37 +374,44 @@ void handShake(){
     keyboardIndex = 1;
     return;
   }
-  
-  //TX_Message[0] = 2;
-  uint8_t RX_Message[8]={0};
+
+  // TX_Message[0] = 2;
+  uint8_t RX_Message[8] = {0};
   uint32_t ID;
   uint32_t start = millis();
   uint32_t current2 = start;
 
-  while (current2 < (start + 3000)){
+  while (current2 < (start + 3000))
+  {
     current2 = millis();
-    if(CAN_CheckRXLevel() > 0){
+    if (CAN_CheckRXLevel() > 0)
+    {
       CAN_RX(ID, RX_Message);
-      if(RX_Message[0] == 1){
+      if (RX_Message[0] == 1)
+      {
         numberOfKeyboards = 3;
-        if(east){
+        if (east)
+        {
           keyboardIndex = 0;
         }
-        else if(west){
+        else if (west)
+        {
           keyboardIndex = 2;
         }
         return;
+      }
     }
-    }
-    
   }
-  
-  if(east || west){
-  numberOfKeyboards = 2;
-    if(east){
-    keyboardIndex = 0;
+
+  if (east || west)
+  {
+    numberOfKeyboards = 2;
+    if (east)
+    {
+      keyboardIndex = 0;
     }
-    else if(west){
+    else if (west)
+    {
       keyboardIndex = 1;
     }
     return;
@@ -432,9 +457,9 @@ void setup()
   xTaskCreate(
       sampleGenerationTask,     /* Function that implements the task */
       "sampleGeneration",       /* Text name for the task */
-      128,                    /* Stack size in words, not bytes */
-      NULL,                  /* Parameter passed into the task */
-      4,                     /* Task priority */
+      128,                      /* Stack size in words, not bytes */
+      NULL,                     /* Parameter passed into the task */
+      4,                        /* Task priority */
       &sampleGenerationHandle); /* Pointer to store the task handle */
 
   TaskHandle_t scanKeysHandle = NULL;
@@ -445,16 +470,15 @@ void setup()
       NULL,             /* Parameter passed into the task */
       3,                /* Task priority */
       &scanKeysHandle); /* Pointer to store the task handle */
-  
-  
+
   TaskHandle_t scanOtherBoards = NULL;
   xTaskCreate(
-      scanOtherBoardsTask,     /* Function that implements the task */
-      "scanOtherBoards",       /* Text name for the task */
-      64,               /* Stack size in words, not bytes */
-      NULL,             /* Parameter passed into the task */
-      2,                /* Task priority */
-      &scanKeysHandle); /* Pointer to store the task handle */
+      scanOtherBoardsTask, /* Function that implements the task */
+      "scanOtherBoards",   /* Text name for the task */
+      64,                  /* Stack size in words, not bytes */
+      NULL,                /* Parameter passed into the task */
+      2,                   /* Task priority */
+      &scanKeysHandle);    /* Pointer to store the task handle */
 
   TaskHandle_t updateDisplayHandle = NULL;
   xTaskCreate(
@@ -482,10 +506,10 @@ void setup()
   // setting knob 3 limits
   knob3.set_limits(0, 8);
 
-  //Initialise CAN
+  // Initialise CAN
   CAN_Init(false);
-  setCANFilter(0x123,0x7ff);
-  CAN_Start();  
+  setCANFilter(0x123, 0x7ff);
+  CAN_Start();
   handShake();
   Serial.println("finished handshaking");
   vTaskStartScheduler();
@@ -494,4 +518,3 @@ void setup()
 void loop()
 {
 }
-
